478616cb052ffdd2e4001ca447f11e02b9763d03
Sanket Parmar <sanketp@hyperlinkinfosystem.net.in>
2023-01-12 16:39:07 +0530

    Initial commit [-] Removed main storyboard. [+] Basic changes added to design via code. [+] Folder structure modified

---
+++ CodeRevisor/CodeRevisor/View/Home/HomeVC.swift
@@ -0,0 +1,16 @@
+//
+//  HomeVC.swift
+//  CodeRevisor
+//
+//  Created by Sanket Parmar on 12/01/23.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
