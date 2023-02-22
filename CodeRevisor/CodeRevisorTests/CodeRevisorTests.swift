29231fc75809ef4cbd7eb711ee3c7d8afc2b4a2a
sanket2126 <sanketparmar2126@gmail.com>
2023-02-22 11:07:30 +0530

    CDCategory - manager all test cases added

--- CodeRevisor/CodeRevisorTests/CodeRevisorTests.swift
+++ CodeRevisor/CodeRevisorTests/CodeRevisorTests.swift
@@ -33,5 +33,59 @@ final class CodeRevisorTests: XCTestCase {
         XCTAssertEqual(data?.count, 1)
         XCTAssertEqual(data?.first?.category, category.category)
     }
+    
+    func testCreateDuplicateCategory_return_Failure() {
+        let uid = UUID()
+        let category = Categories(category: "Test", id: uid)
+        
+        let uid2 = UUID()
+        let category2 = Categories(category: "te st ", id: uid2)
+        
+        let manager = CategoryManager(persistantStorage: persistentStorage)
+        manager.createCategory(category: category)
+        
+        manager.createCategory(category: category2)
+        
+        let data = persistentStorage.fetchManagedObject(managedObject: CDCategory.self)
+        
+        XCTAssertEqual(data?.count, 1)
+    }
+    
+    func testFetchCategory_return_Failure() {
+        let data = persistentStorage.fetchManagedObject(managedObject: CDCategory.self)
+        
+        XCTAssertEqual(data?.count, 0)
+    }
+    
+    func testUpdateCategory_return_Success() {
+        let uid = UUID()
+        let category = Categories(category: "Test", id: uid)
+        
+        let manager = CategoryManager(persistantStorage: persistentStorage)
+        manager.createCategory(category: category)
+        
+        let updateCategory = Categories(category: "Updated Test", id: uid)
+        let result = manager.updateCategory(category: updateCategory)
+        
+        let data = persistentStorage.fetchManagedObject(managedObject: CDCategory.self)
+        
+        XCTAssertEqual(result, true)
+        XCTAssertEqual(data?.first?.category, updateCategory.category)
+    }
+    
+    func testDeleteCategory_return_Success() {
+        let uid = UUID()
+        let category = Categories(category: "Test", id: uid)
+        
+        let manager = CategoryManager(persistantStorage: persistentStorage)
+        manager.createCategory(category: category)
+        
+        let result = manager.deleteCategory(id: category.id)
+        
+        let data = persistentStorage.fetchManagedObject(managedObject: CDCategory.self)
+        
+        XCTAssertEqual(result, true)
+        XCTAssertEqual(data?.count, 0)
+    }
 
 }
